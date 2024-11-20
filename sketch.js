236764a7145f0f19879cd91959b1187cdec33e17
Globby Bobby <supernageladam@icloud.com>
2024-11-20 14:01:05 -0600

    Updated Final Project

--- sketch.js
+++ sketch.js
@@ -5,7 +5,15 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let canvas;
+
 let hitboxArray = [];
+
+let playerX = 200;
+let playerY = 200;
+const DEFAULT_PLAYER_MOVESPEED = 3;
+const SHIFT_PLAYER_MOVESPEED = 1;
+let playerMoveSpeed = DEFAULT_PLAYER_MOVESPEED;
 
 //htibox class 
 class Hitbox {
@@ -24,18 +32,48 @@ class Hitbox {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowHeight/3*4, windowHeight);
+  //shake left and right when bomb's explosion 'ticks'
+  canvas.position((windowWidth-width)/2,0);
+  frameRate(60);
+}
+
+function checkInput() {
+  if (keyIsPressed) {
+    //slow down player movement if shift is held
+    if (keyIsDown(SHIFT)) {
+      playerMoveSpeed = SHIFT_PLAYER_MOVESPEED;
+    }
+    else {
+      playerMoveSpeed = DEFAULT_PLAYER_MOVESPEED;
+    }
+    //move player with arrow keys
+    if (keyIsDown(RIGHT_ARROW)) {
+      playerX += playerMoveSpeed;
+    };
+    if (keyIsDown(LEFT_ARROW)) {
+      playerX -= playerMoveSpeed;
+    };
+    if (keyIsDown(UP_ARROW)) {
+      playerY -= playerMoveSpeed;
+    };
+    if (keyIsDown(DOWN_ARROW)) {
+      playerY += playerMoveSpeed;
+    };
+  }
 }
 
 function draw() {
   background(220);
-  if (frameCount % 30 === 0) {
-    let x1 = random(0,width);
-    let y1 = random(0,height);
-    let hitbox = new Hitbox(x1,y1,x1+0.1,y1+1,'none');
-    hitboxArray.push(hitbox);
-  }
-  for (let item of hitboxArray) {
-    item.draw();
-  }
+  checkInput();
+  square(playerX,playerY,5);
+  // if (frameCount % 30 === 0) {
+  //   let x1 = random(0,width);
+  //   let y1 = random(0,height);
+  //   let hitbox = new Hitbox(x1,y1,x1+0.1,y1+1,'none');
+  //   hitboxArray.push(hitbox);
+  // }
+  // for (let item of hitboxArray) {
+  //   item.draw();
+  // }
 }
